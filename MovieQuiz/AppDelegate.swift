e93dc91e80a34f64870fedffcc8f37f5e92bc639
ArtemP878 <113448942+ArtemP878@users.noreply.github.com>
2023-02-14 01:17:25 +0400

    поправил 5 спринт

--- MovieQuiz/AppDelegate.swift
+++ MovieQuiz/AppDelegate.swift
@@ -1,9 +1,3 @@
-//
-//  AppDelegate.swift
-//  MovieQuiz
-//
-//  Created by Constantine Nikolsky on 15.05.2022.
-//
 
 import UIKit
 
@@ -11,8 +5,7 @@ import UIKit
 final class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        print("Hello world for branch to merge")
-        return true
+            return true
     }
 
     // MARK: UISceneSession Lifecycle
@@ -27,5 +20,6 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+
     }
 }
--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,11 +1,4 @@
 import Foundation
 import UIKit
 
-extension UIColor {
-    static var ypBlack: UIColor { UIColor(named: "Black")!}
-    static var ypBackground: UIColor { UIColor(named: "Background")!}
-    static var ypGray: UIColor { UIColor(named: "Gray")!}
-    static var ypGreen: UIColor { UIColor(named: "Green")!}
-    static var ypRed: UIColor { UIColor(named: "Red")!}
-    static var ypWhite: UIColor { UIColor(named: "White")!}
-}
+extension UIColor { }
